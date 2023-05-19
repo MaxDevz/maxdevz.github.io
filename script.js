bc1574266dd99395a8410518218990940ad9fa05
MaximeStOnge <49131960+MaxDevz@users.noreply.github.com>
2023-05-19 09:42:31 -0400

    Add logo and pages

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-function myFunction() {
-  document.getElementById("demo").innerHTML = "Paragraph changed.";
-}
+const app = {
+  init: () => {
+    document.addEventListener("DOMContentLoaded", app.load);
+    console.log("HTML loaded");
+  },
+  load: () => {
+    app.loadPage();
+  },
+
+  loadPage: (url) => {
+    url = "./pages/seasons.html";
+    const contentDiv = document.getElementById("page");
+    fetch(url)
+      .then((res) => {
+        if (res.ok) {
+          console.log(res);
+          return res.text();
+        }
+      })
+      .then((htmlPage) => {
+        contentDiv.innerHTML = htmlPage;
+      });
+  },
+};
+app.init();
